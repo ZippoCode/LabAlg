8d9911c06f4f6534f79462adba8b62011bd4c5a1
Salvatore Prochilo <prochilo.salvatore@gmail.com>
2016-10-27 00:44:53 +0200

    Add Icon Selection Griglia e Resolution Bug Fix

--- labalg/src/progetto/gui/JFrameSceltaGriglia.java
+++ labalg/src/progetto/gui/JFrameSceltaGriglia.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -31,32 +32,36 @@ public class JFrameSceltaGriglia extends JFrame {
 		this.mediator = mediator;
 		pannello.setLayout(new GridLayout(1, 1));
 		JTabbedPane tabella = new JTabbedPane();
-		tabella.add("3", creaPannello(GetStringa.numTre, 3));
+		tabella.add("    3    ", creaPannello(GetStringa.numTre, 3));
 		tabella.setMnemonicAt(0, KeyEvent.VK_3);
-		tabella.add("4", creaPannello(GetStringa.numQuattro, 4));
+		tabella.add("    4    ", creaPannello(GetStringa.numQuattro, 4));
 		tabella.setMnemonicAt(1, KeyEvent.VK_4);
-		tabella.add("5", creaPannello(GetStringa.numCinque, 5));
+		tabella.add("    5    ", creaPannello(GetStringa.numCinque, 5));
 		tabella.setMnemonicAt(2, KeyEvent.VK_5);
-		tabella.add("6", creaPannello(GetStringa.numSei, 6));
+		tabella.add("    6    ", creaPannello(GetStringa.numSei, 6));
 		tabella.setMnemonicAt(3, KeyEvent.VK_6);
-		tabella.add("7", creaPannello(GetStringa.numSette, 7));
+		tabella.add("    7    ", creaPannello(GetStringa.numSette, 7));
 		tabella.setMnemonicAt(4, KeyEvent.VK_7);
-		tabella.add("8", creaPannello(GetStringa.numOtto, 8));
+		tabella.add("    8    ", creaPannello(GetStringa.numOtto, 8));
 		tabella.setMnemonicAt(5, KeyEvent.VK_8);
-		tabella.add("9", creaPannello(GetStringa.numNove, 9));
+		tabella.add("    9    ", creaPannello(GetStringa.numNove, 9));
 		tabella.setMnemonicAt(6, KeyEvent.VK_9);
-		pannello.add(tabella);
 		tabella.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		tabella.setBackground(Color.WHITE);
+		pannello.add(tabella);
+		pannello.setBackground(Color.WHITE);
 		add(pannello);
 		pack();
 		setVisible(false);
-		setSize(new Dimension(400, 400));
+		Dimension dimScreen = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(new Dimension(dimScreen.width / 3, dimScreen.height / 2));
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 	}
 
 	private JScrollPane creaPannello(int numero, int dimensione) {
 		JPanel pannello = new JPanel(new GridLayout(numero, 2, 10, 10));
+		pannello.setBackground(Color.WHITE);
 		JButton[] v = new JButton[numero];
 		for (int i = 0; i < v.length; i++) {
 			try {
@@ -64,9 +69,14 @@ public class JFrameSceltaGriglia extends JFrame {
 				BufferedImage background = ImageIO.read(getClass().getClassLoader().getResource(stringa));
 				Image resizeBackground = background.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
 				ImageIcon icona = new ImageIcon(resizeBackground);
+				BufferedImage iconaGame = ImageIO.read(getClass().getClassLoader().getResource("info/play game.png"));
+				Image iconaGameResize = iconaGame.getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+				ImageIcon ig = new ImageIcon(iconaGameResize);
 				pannello.add(new JLabel(icona));
-				v[i] = new JButton("Seleziona");
+				v[i] = new JButton(ig);
 				v[i].setBackground(Color.WHITE);
+				v[i].setBorder(null);
+				v[i].setSelected(false);
 				v[i].setName(GetStringa.getStringa(dimensione, i + 1));
 				mediator.manageEvent(new ActionEvent(v[i], Counter.generateID(), null));
 				pannello.add(v[i]);
